28c4ba6b96ab482d78d1d902b6e14ef59e7bfb59
rosanenicacio <39062505+rosanenicacio@users.noreply.github.com>
2021-05-10 20:35:54 -0300

    Add files via upload

---
+++ laco_for_lista.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+lista = [1, 2, 3, 15, 12, 10, 8]
+
+
+# In[19]:
+
+
+for numero in lista:
+       if (numero % 2 == 0):
+            print(numero) 
+
+
+# In[ ]:
+
+
+
+
